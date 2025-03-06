c66cd6548c54c95a7cc311f0ea306b3647b9ea47
Rohan Tongle <rohan.tongle@neyyarapp.com>
2025-03-06 14:43:26 +0530

    update asset references in index.html and add new JavaScript files

---
+++ react-app/assets/src-UW24ZMRV-DVmSgXUr.js
@@ -0,0 +1,1 @@
+import{d as a}from"./features-animation-BIv4pZ7e.js";import"./index-Ci7dCAQ4.js";var t=a;export{t as default};
--- react-app/assets/src-UW24ZMRV-rksTKWEt.js
+++
@@ -1,1 +0,0 @@
-import{d as a}from"./features-animation-Bmnt0iuk.js";import"./index-BtEiJsT8.js";var t=a;export{t as default};
