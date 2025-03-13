4937ed49af3b833066dbee89b0bc5afa828600d2
Rohan Tongle <rohan.tongle@neyyarapp.com>
2025-03-13 14:14:17 +0530

    refactor: update asset files and modify ChatPopup component for improved styling and functionality

---
+++ react-app/assets/index-BpZDqlG0.js
@@ -0,0 +1,1 @@
+import{d as a}from"./features-animation-C9JFbUnJ.js";import"./index-kLKtf6yQ.js";var d=a;export{d as default};
--- react-app/assets/src-UW24ZMRV-DJ1yp5q9.js
+++
@@ -1,1 +0,0 @@
-import{d as a}from"./features-animation-Tc6_3hsE.js";import"./index-Mhx0bxQS.js";var t=a;export{t as default};
