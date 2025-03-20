256d9de825d8c6c72cd8a6cf96c7ea0c2ad7dd67
Sajinsha <84446813+sajinsha@users.noreply.github.com>
2025-03-20 14:36:13 +0530

    chatbot fix

--- react-app/assets/index-XkvsjBWX.js
+++
@@ -1,1 +0,0 @@
-import{d as a}from"./features-animation-CkYo5qkH.js";import"./index-DBmZPbpp.js";var d=a;export{d as default};
--- react-app/assets/src-UW24ZMRV-C_kPKcBJ.js
+++
@@ -1,1 +0,0 @@
-import{d as a}from"./features-animation-CkYo5qkH.js";import"./index-DBmZPbpp.js";var t=a;export{t as default};
